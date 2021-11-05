c39e5d529a4921b93c51dcb0704bd3ead8f5b3a7
ahnafabir <ahnafabir313@gmail.com>
2021-11-05 20:00:19 +0600

    404 page code lined up

--- src/pages/404/WrongUrl.js
+++ src/pages/404/WrongUrl.js
@@ -1,19 +1,19 @@
 import "./WrongUrl.css";
 import React, { useEffect } from "react";
-import Container from "@mui/material/Container";
+import { useNavigate } from "react-router";
 import errorImg from "../../images/page-not-found-img.png";
 import Button from "@mui/material/Button";
-import { useNavigate } from "react-router";
 
 const WrongUrl = ({ setPageTitle }) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+
   useEffect(() => {
-    setPageTitle("Page Not Found");
-  }, []);
+    setPageTitle("Page Not Found")
+  }, [])
 
   return (
     <div className="WrongUrl text-center">
-      <img src={errorImg} alt="" />
+      <img src={errorImg} alt="dyno book 404 not found page" />
       <h1>404 - PAGE NOT FOUND</h1>
       <p className="mb-0">The page you are looking for might have been removed </p>
       <p>had it's name changed or is temporarily unavailable</p>
