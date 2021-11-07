e71518f219d77ccc92f270e09149666cb220d7c9
ahnafabir <ahnafabir313@gmail.com>
2021-11-07 16:01:13 +0600

    fixed styling issue of about us page and new header style applied

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import "./Header.css";
 import React, { useState } from "react";
-import { IconButton, Typography, Button, Drawer } from "@mui/material";
-import { Box, List, ListItem } from "@mui/material";
-import { MdOutlineMapsHomeWork } from "react-icons/md";
-import { ImSortNumbericDesc } from "react-icons/im";
 import { Link } from "react-router-dom";
+import { IconButton, Typography, Button, Drawer } from "@mui/material";
+import { List, ListItemButton, ListItemText, Collapse } from "@mui/material";
+import { MdOutlineMapsHomeWork } from "react-icons/md";
+import { MdExpandMore, MdExpandLess } from 'react-icons/md';
 
 const Header = () => {
-  const [openVersityDrawer, setOpenVersityDrawer] = useState(false);
-  const [openYearDrawer, setOpenYearDrawer] = useState(false);
+  const [openVersityDrawer, setOpenVersityDrawer] = useState(false)
+  const [open, setOpen] = useState(false)
 
   return (
-    <nav className="h-1">
+    <nav className="h-14">
       <div className="bg-white flex items-center border-b border-gray fixed w-full z-10">
         <IconButton
           size="large"
@@ -24,13 +24,12 @@ const Header = () => {
           <MdOutlineMapsHomeWork color="#000" />
         </IconButton>
 
-        <Typography noWrap variant="h6" component="div" sx={{ ml: 1 }}>
+        <Typography noWrap variant="h6" component="div" sx={{ ml: 1, textAlign: { xs: 'center', sm: 'left' }, flex: 1 }}>
           <Link to="/" className="text-decoration-none text-black">
             DYNO BOOK
           </Link>
         </Typography>
 
-        <Box flexGrow="1" />
 
         <input
           type="text"
@@ -38,20 +37,11 @@ const Header = () => {
           className="hearder_search_box"
         />
 
-        <Button variant="contained">
-          <Typography noWrap>add new question</Typography>
-        </Button>
-
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          sx={{ mx: { xs: 0, md: 2 } }}
-          onClick={() => setOpenYearDrawer(true)}
-        >
-          <ImSortNumbericDesc />
-        </IconButton>
+        <div className="hidden sm:block">
+          <Button variant="contained">
+            <Typography noWrap>add new question</Typography>
+          </Button>
+        </div>
       </div>
 
       {/* Versity Drawer */}
@@ -60,35 +50,82 @@ const Header = () => {
         open={openVersityDrawer}
         onClose={() => setOpenVersityDrawer(false)}
       >
-        <h6 className="text-center py-2">List Of Univerisies</h6>
-        <List
-          style={{ minWidth: "230px" }}
-          onClick={() => setOpenVersityDrawer(false)}
-        >
-          <ListItem button>Chittagong University</ListItem>
-          <ListItem button>Comilla University</ListItem>
-          <ListItem button>Dahaka University</ListItem>
-          <ListItem button>Rajshahi University</ListItem>
-          <ListItem button>Shajalal University</ListItem>
-        </List>
-      </Drawer>
+        <h6 className="h-14 font-bold border-b flex items-center justify-center">List Of Universities</h6>
+        <List>
+          <ListItemButton onClick={() => setOpen(!open)}>
+            <ListItemText>Chittagong University</ListItemText>
+            <div className="ml-2">{open ? <MdExpandLess /> : <MdExpandMore />}</div>
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List className="text-right">
+              <ListItemButton>2020-2021</ListItemButton>
+              <ListItemButton>2019-2020</ListItemButton>
+              <ListItemButton>2018-2019</ListItemButton>
+              <ListItemButton>2017-2018</ListItemButton>
+              <ListItemButton>2016-2017</ListItemButton>
+              <ListItemButton>2015-2016</ListItemButton>
+            </List>
+          </Collapse>
 
-      {/* Academic Year Drawer */}
-      <Drawer
-        anchor="right"
-        open={openYearDrawer}
-        onClose={() => setOpenYearDrawer(false)}
-      >
-        <h6 className="text-center py-2">Academic Year</h6>
-        <List
-          style={{ minWidth: "230px" }}
-          onClick={() => setOpenYearDrawer(false)}
-        >
-          <ListItem button>2020 - 2021</ListItem>
-          <ListItem button>2019 - 2020</ListItem>
-          <ListItem button>2018 - 2019</ListItem>
-          <ListItem button>2017 - 2018</ListItem>
-          <ListItem button>2016 - 2017</ListItem>
+          <ListItemButton onClick={() => setOpen(!open)}>
+            <ListItemText>Comilla University</ListItemText>
+            {open ? <MdExpandLess /> : <MdExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List className="text-right">
+              <ListItemButton>2020-2021</ListItemButton>
+              <ListItemButton>2019-2020</ListItemButton>
+              <ListItemButton>2018-2019</ListItemButton>
+              <ListItemButton>2017-2018</ListItemButton>
+              <ListItemButton>2016-2017</ListItemButton>
+              <ListItemButton>2015-2016</ListItemButton>
+            </List>
+          </Collapse>
+
+          <ListItemButton onClick={() => setOpen(!open)}>
+            <ListItemText>Dhaka University</ListItemText>
+            {open ? <MdExpandLess /> : <MdExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List className="text-right">
+              <ListItemButton>2020-2021</ListItemButton>
+              <ListItemButton>2019-2020</ListItemButton>
+              <ListItemButton>2018-2019</ListItemButton>
+              <ListItemButton>2017-2018</ListItemButton>
+              <ListItemButton>2016-2017</ListItemButton>
+              <ListItemButton>2015-2016</ListItemButton>
+            </List>
+          </Collapse>
+
+          <ListItemButton onClick={() => setOpen(!open)}>
+            <ListItemText>Rajshahi University</ListItemText>
+            {open ? <MdExpandLess /> : <MdExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List className="text-right">
+              <ListItemButton>2020-2021</ListItemButton>
+              <ListItemButton>2019-2020</ListItemButton>
+              <ListItemButton>2018-2019</ListItemButton>
+              <ListItemButton>2017-2018</ListItemButton>
+              <ListItemButton>2016-2017</ListItemButton>
+              <ListItemButton>2015-2016</ListItemButton>
+            </List>
+          </Collapse>
+
+          <ListItemButton onClick={() => setOpen(!open)}>
+            <ListItemText>Sha Jalal University</ListItemText>
+            {open ? <MdExpandLess /> : <MdExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List className="text-right">
+              <ListItemButton>2020-2021</ListItemButton>
+              <ListItemButton>2019-2020</ListItemButton>
+              <ListItemButton>2018-2019</ListItemButton>
+              <ListItemButton>2017-2018</ListItemButton>
+              <ListItemButton>2016-2017</ListItemButton>
+              <ListItemButton>2015-2016</ListItemButton>
+            </List>
+          </Collapse>
         </List>
       </Drawer>
     </nav>
--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,124 +1,83 @@
 import "./AboutUs.css";
 import React, { useEffect } from "react";
-import {
-  Accordion,
-  AccordionSummary,
-  Typography,
-  AccordionDetails,
-} from "@mui/material";
+import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
 import { MdExpandMore } from "react-icons/md";
 
 const AboutUs = ({ setPageTitle }) => {
   useEffect(() => {
-    setPageTitle("About Us | Dyno Book");
-  }, []);
+    setPageTitle("About Us | Dyno Book")
+  }, [])
 
   return (
-    <main className="container mt-16 mx-auto  px-4 ">
-      <section>
-        <div className="mb-7">
-          <h1 className=" mb-3 text-2xl md:text-4xl lg:text-5xl font-semibold">
-            What is Dyno Book ?
-          </h1>
-          <p className="text-justify xs:font-medium md:text-lg">
-            Dyno Book comes form two different words - Dynamic and Book. Yeah!
-            You have guessed it right. Dyno Book means dynamic book. But why do
-            we call it dynamic or what made it dynamic? And why do we call this
-            website - a book? Well, let's answer the second question first. The
-            answer is quite simple. We provide all the types of question here
-            that you get from books. But the difference is you need to buy the
-            books for those questions and we provide those for free. And we made
-            our website's UI (User Interface) so simple that you will feel; you
-            are reading a book not browsing a website. I hope you have got your
-            second question's answer. Now, let's explain the first question!
-            Dynamic Book means you can edit your book as per your preferences.
-            For example, By default you don't want to see the answers of any
-            question; you can hide the answers of your book. You will get
-            unlimited random questions to test yourself. You can filter your
-            question as per your preference. Just think about can you filter
-            your physical book. There are lots of cool features like these. You
-            need to just explore the website for a while.
-          </p>
-        </div>
-        <div>
-          <h1 className="mb-3 md:mb-6 text-2xl md:text-4xl lg:text-5xl font-semibold">
-            Why Would I Use Dyno Book ?
-          </h1>
-          <p className="text-justify xs:font-medium md:text-lg">
-            As we already said Dyno Book is completely free. You don't need to
-            spend a single buck to use it, you don't need to get any
-            subscription for it whereas you have to buy books from any book
-            store. Even you don't need to create an account like other websites
-            and it's easy to use. So, if you can read books here without buying
-            and it's easy to use why you won't use Dyno Book. But These are not
-            the only reason for using Dyno Book. The features of Dyno Book made
-            itself awesome. Some current features and reason have been explained
-            below -
-          </p>
-        </div>
+    <main className="container mx-auto px-2">
+      <section className="mt-4 sm:mt-8">
+        <h1 className="mb-3 text-2xl font-semibold xs:font-bold sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl">
+          What is Dyno Book ?
+        </h1>
+
+        <p className="text-justify text-sm xs:text-base md:text-md lg:text-lg 2xl:text-xl">
+          Dyno Book comes form two different words - Dynamic and Book. Yeah! You have guessed it right. Dyno Book means dynamic book. But why do we call it dynamic or what made it dynamic? And why do we call this website - a book? Well, let's answer the second question first. The answer is quite simple. We provide all the types of question here that you get from books. But the difference is you need to buy the books for those questions and we provide those for free. And we made our website's UI (User Interface) so simple that you will feel; you are reading a book not browsing a website. I hope you have got your second question's answer. Now, let's explain the first question! Dynamic Book means you can edit your book as per your preferences. For example, By default you don't want to see the answers of any question; you can hide the answers of your book. You will get unlimited random questions to test yourself. You can filter your question as per your preference. Just think about can you filter your physical book. There are lots of cool features like these. You need to just explore the website for a while.
+        </p>
       </section>
+
+      <section className="mt-10 sm:mt-14">
+        <h1 className="mb-3 text-2xl font-semibold xs:font-bold sm:text-3xl md:mb-6 md:text-4xl lg:text-5xl 2xl:text-6xl">
+          Why Would I Use Dyno Book ?
+        </h1>
+
+        <p className="text-justify text-sm xs:text-base md:text-md lg:text-lg 2xl:text-xl">
+          As we already said Dyno Book is completely free. You don't need to spend a single buck to use it, you don't need to get any
+          subscription for it whereas you have to buy books from any book store. Even you don't need to create an account like other websites and it's easy to use. So, if you can read books here without buying and it's easy to use why you won't use Dyno Book. But These are not the only reason for using Dyno Book. The features of Dyno Book made itself awesome. Some current features and reason have been explained below -
+        </p>
+      </section>
+
       <section className="my-10">
-        <div className="mb-5">
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<MdExpandMore />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography>It's free and easy to use</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography className="text-justify">
-                You don't need to send your money to use it. There is no premium
-                content here. All the contents here are completely free for
-                everybody. It's really easy to use, you will not face any
-                trouble to it. We have tried to make it easy to navigate as much
-                as possible. Most importantly you don't need to create any account
-                to use it.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </div>
-        <div className="mb-5">
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<MdExpandMore />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography>Generate question paper</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography className="text-justify">
-                Generating question paper is one of our best features. You can
-                generate a question paper for a specific examination. The
-                question will be random. You will not get the same question
-                again and again. You can also filter question category to
-                generate. For example - you select only grammar category and you
-                will get a question paper base on grammar.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </div>
-        <div className="mb-5">
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<MdExpandMore />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography>Accordion 1</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography className="text-justify">
-                Question filtering is one of the strongest feature of Dyno Book.
-                You will find almost all type of category to filter your
-                question. For example you can filter by institution, academic
-                year, subject, specific topic of any subject.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<MdExpandMore />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <h4 className="md:font-semibold lg:text-lg xl:text-xl">It's free and easy to use</h4>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p className="text-justify text-sm md:text-base">
+              You don't need to send your money to use it. There is no premium content here. All the contents here are completely free for everybody. It's really easy to use, you will not face any trouble to it. We have tried to make it easy to navigate as much as possible. Most importantly you don't need to create any account to use it.
+            </p>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<MdExpandMore />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <h4 className="md:font-semibold lg:text-lg xl:text-xl">Generate question paper</h4>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p className="text-justify text-sm md:text-base">
+              Generating question paper is one of our best features. You can generate a question paper for a specific examination. The
+              question will be random. You will not get the same question again and again. You can also filter question category to generate. For example - you select only grammar category and you will get a question paper base on grammar.
+            </p>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<MdExpandMore />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <h4 className="md:font-semibold lg:text-lg xl:text-xl">Question filtering</h4>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p className="text-justify text-sm md:text-base">
+              Question filtering is one of the strongest feature of Dyno Book. You will find almost all type of category to filter your
+              question. For example you can filter by institution, academic year, subject, specific topic of any subject.
+            </p>
+          </AccordionDetails>
+        </Accordion>
       </section>
     </main>
   );
