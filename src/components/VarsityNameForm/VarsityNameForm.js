6490c159bcc0ffb6502e7a98f1f839229a266d4e
ahnafabir <ahnafabir313@gmail.com>
2021-11-12 03:50:06 +0600

    name form complete in update varsity page

---
+++ src/components/VarsityNameForm/VarsityNameForm.js
@@ -0,0 +1,79 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { VarsitiesInfo } from '../../App';
+import { TextField, Button } from '@mui/material';
+
+const VarsityNameForm = ({ varsity, setSnackbarOpen, setAlertType, setAlertMessage }) => {
+  const [, setVarsitiesInfo] = useContext(VarsitiesInfo)
+  const [name, setName] = useState("")
+  const [shortName, setShortName] = useState("")
+
+  useEffect(() => {
+    setName("")
+    setShortName("")
+  }, [varsity])
+
+  const updateVarsityName = (e) => {
+    e.preventDefault();
+
+    let value = { id: varsity._id }
+    if (name !== "") value = { ...value, name }
+    if (shortName !== "") value = { ...value, shortName }
+
+    const isValid = Object.keys(value)
+
+    if (isValid.length > 1) {
+      fetch("http://localhost:5000/api/varsities/update/name", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(value)
+      })
+        .then(res => res.json())
+        .then(data => {
+          fetch("http://localhost:5000/api/varsities")
+            .then(res => res.json())
+            .then(data => setVarsitiesInfo(data.data))
+        })
+
+      setSnackbarOpen(true)
+      setAlertType('success')
+      setAlertMessage("Varsity Info Updated")
+
+    } else {
+      setSnackbarOpen(true)
+      setAlertType("error")
+      setAlertMessage("minimum one field need to be changed")
+    }
+  }
+
+  return (
+    <div className="max-w-4xl mx-auto px-3">
+      <h1 className="text-center mt-6 text-xl sm:text-3xl">UPDATE VARSITY NAME</h1>
+      <form
+        onSubmit={updateVarsityName}
+        className='mt-8 flex flex-col gap-5 sm:flex-row'
+      >
+        <TextField
+          name="fname"
+          autoComplete="off"
+          placeholder={varsity.name}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          sx={{ flexGrow: 1 }}
+
+        />
+
+        <TextField
+          name="sname"
+          autoComplete="off"
+          placeholder={varsity.shortName}
+          value={shortName}
+          onChange={(e) => setShortName(e.target.value)}
+        />
+
+        <Button variant="contained" type="submit">update name</Button>
+      </form>
+    </div>
+  );
+};
+
+export default VarsityNameForm;
--- src/pages/UpdateVarsity/UpdateVarsity.js
+++ src/pages/UpdateVarsity/UpdateVarsity.js
@@ -1,18 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { PageTitle } from '../../App';
+import { Alert, Snackbar } from '@mui/material';
+import VarsityNameForm from '../../components/VarsityNameForm/VarsityNameForm';
 
 const UpdateVarsity = () => {
   const { id } = useParams()
 
+  const [, setPageTitle] = useContext(PageTitle)
+
   const [varsity, setVarsity] = useState({})
+  const [snackbarOpen, setSnackbarOpen] = useState(false)
+  const [alertType, setAlertType] = useState("error")
+  const [alertMessage, setAlertMessage] = useState("")
+
+  useEffect(() => setPageTitle("Edit | University Info"))
 
   useEffect(() => {
-
+    fetch(`http://localhost:5000/api/varsities/${id}`)
+      .then(res => res.json())
+      .then(data => setVarsity(data.data[0]))
   }, [id])
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') return
+    setSnackbarOpen(false)
+  }
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-lg">{id}</h1>
+      <VarsityNameForm
+        varsity={varsity}
+        setSnackbarOpen={setSnackbarOpen}
+        setAlertType={setAlertType}
+        setAlertMessage={setAlertMessage}
+
+      />
+
+      {/* wrong credential alert */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={5000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert severity={alertType} onClose={handleSnackbarClose}>{alertMessage}</Alert>
+      </Snackbar>
     </div>
   );
 };
